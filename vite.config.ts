a840b6fdc00c455dafb81621c13c42e88ab28c46
Gabriel-Sousa-Dev <gabrieldesousa.dev@gmail.com>
2024-12-24 13:28:11 -0300

    Configuração feita

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'node:path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "./",
+  
+  resolve: {
+    alias: {
+      "@Components": path.resolve(__dirname,  "./src/components/"),
+      
+    }
+  },
+  build: {
+    outDir: './dist-react'
+  }
 })
